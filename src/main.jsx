536fdf405ac47526a27ac716f384a2c65a4eb8b9
Peter Mikhaylutsa <petercash@mail.ru>
2024-02-12 23:59:22 +0300

    deployed to gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,8 @@ import ReactDOM from "react-dom/client";
 import App from "./components/app/App";
 import "./style/style.scss";
 import { BrowserRouter as Router } from "react-router-dom";
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Router basename="/react-fastfood">
-      <App />
-    </Router>
+  <Router>
+    <App />
+  </Router>
 );
