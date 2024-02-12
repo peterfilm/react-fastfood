45d3aff3ec1824081998bdea9377de79b21a425e
Peter Mikhaylutsa <petercash@mail.ru>
2024-02-13 00:15:05 +0300

    Updates

---
+++ assets/Preloader--ki3rTso.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-qXroa3BU.js";const e="/react-fastfood/assets/cooking_preloader-_UV4iyrZ.gif",a=()=>r.jsx("div",{className:"container",children:r.jsx("div",{className:"preloader",children:r.jsx("img",{src:e,alt:"Preloader"})})});export{a as P};
--- assets/Spinner-e0AtvOsV.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-rsCI6zH1.js";const n="/react-fastfood/assets/spinner-taRkA8vQ.gif",e=()=>s.jsx("div",{className:"spinner",children:s.jsx("div",{className:"container",children:s.jsx("div",{className:"spinner__wrapper",children:s.jsx("img",{src:n,alt:"Loading...",className:"spinner__img"})})})});export{e as default};
